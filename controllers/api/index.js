a4e443be6cd0c4997c4ebade2b1c3bfefe833699
Tiffany <leopardmitai@gmail.com>
2022-03-30 15:32:14 -0700

    started adding routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
+const userRoutes = require("./userRoutes");
+const postRoutes = require("./postRoutes");
+const commentRoutes = require("./commentRoutes");
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use("/users", userRoutes);
+router.use("/posts", postRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
